0059d6bd4af7222009f6f816089a6feed3dbcd42
Sarverun Simeon Tertese <simmyt3r@gmail.com>
2026-09-26 07:20:28 +0100

    Add shareable permalinks to every Builder lesson

--- builder-sw.js
+++ builder-sw.js
@@ -1,4 +1,4 @@
-const CACHE='builder-v6';
+const CACHE='builder-v7';
 const CORE=['./builder.html','./builder.webmanifest','./formal.jpeg','./casual.jpg','./lessons/chatgpt-sites-small-business.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
